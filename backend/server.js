5424093eb2b5860905a26555faf5d124710c74c0
rana1506 <rana1506@gmail.com>
2026-03-20 20:56:39 +0000

    second commit

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.end('Hello, World! Eid Mubarak\n');
 });
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
-});
+}); 
